841bf799980bbf0b9853c7a5c06aa3e8af5309f1
Oxqlion <88870464+oxqlion@users.noreply.github.com>
2024-05-30 17:36:27 +0700

    update on homepage set plan view

--- NutriMate_v1/View/iOS/iOSHomepageView.swift
+++ NutriMate_v1/View/iOS/iOSHomepageView.swift
@@ -44,18 +44,34 @@ struct SheetView: View {
     @State private var target: String = ""
     @State private var selectedDate = Date()
     
-    @State private var selectedOptions: [String] = []
-    private let options = ["+ 🥬 Vegetables", "+ 🍉 Fruits", "+ 🐟 Proteins", "+ 🥛 Milk", "+ 🫚 Herbs"]
+    @State private var selectedOptions: [String] = ["","","","",""]
+    private let options = ["🥬 Vegetables", "🍉 Fruits", "🐟 Proteins", "🥛 Milk", "🫚 Herbs"]
     
     
     var body: some View {
-        VStack {
+        ScrollView {
             
-            Text("Create new plan")
-            
-            Form {
+            VStack {
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .frame(width: 80, height: 10)
+                    .cornerRadius(100)
+                    .padding()
+                
+                Text("Create new plan")
+                
                 Section(header: Text("Input Weigh Loss (kg)")) {
-                    TextField("target", text: $target)
+                    TextField("Target", text: $target)
+                        .padding()
+                        .background(.white)
+                        .cornerRadius(10)
+                        .onChange(of: target) { newValue in
+                            let filtered = newValue.filter { "0123456789".contains($0) }
+                            if target != filtered {
+                                target = filtered
+                            }
+                        }
+                    
                 }
                 
                 Section(header: Text("Due Date")) {
@@ -63,54 +79,111 @@ struct SheetView: View {
                         .datePickerStyle(GraphicalDatePickerStyle())
                 }
                 
-                Section(header: Text("Select Options")) {
-                    ForEach(options, id: \.self) { option in
-                        Button(action: {
-                            if let index = selectedOptions.firstIndex(of: option) {
-                                selectedOptions.remove(at: index)
+                HStack {
+                    Text("+ 🥬 Vegetables")
+                        .padding()
+                        .background(selectedOptions.contains("Vegetables") ? Color(hex: 0xD3D3D3) : Color.white)
+                        .cornerRadius(100)
+                        .onTapGesture {
+                            if selectedOptions.contains("Vegetables") {
+                                selectedOptions[0] = ""
                             } else {
-                                selectedOptions.append(option)
-                            }
-                        }) {
-                            HStack {
-                                Text(option)
-                                    .foregroundColor(.white)
-                                    .padding()
-                                    .background(selectedOptions.contains(option) ? Color.green : Color.blue)
-                                    .cornerRadius(100)
-                            }
+                                self.selectedOptions[0] = "Vegetables"                            }
                         }
-                        .padding(.vertical, 4)
-                    }
+                    
+                    Text("+ 🍉 Fruits")
+                        .padding()
+                        .background(selectedOptions.contains("Fruits") ? Color(hex: 0xD3D3D3) : Color.white)
+                        .cornerRadius(100)
+                        .onTapGesture {
+                            if selectedOptions.contains("Fruits") {
+                                selectedOptions[1] = ""
+                            } else {
+                                self.selectedOptions[1] = "Fruits"                            }
+                        }
+                    Spacer()
                 }
-            }
-            .cornerRadius(10)
-            .padding(.horizontal)
-            
-            Text("Selected Options: \(selectedOptions.joined(separator: ", "))")
-                .padding()
-            
-            Button{
                 
-            } label: {
-                Text("Done")
-                    .foregroundColor(.white)
+                HStack {
+                    
+                    Text("+ 🫚 Herbs")
+                        .padding()
+                        .background(selectedOptions.contains("Herbs") ? Color(hex: 0xD3D3D3) : Color.white)
+                        .cornerRadius(100)
+                        .onTapGesture {
+                            if selectedOptions.contains("Herbs") {
+                                selectedOptions[2] = ""
+                            } else {
+                                self.selectedOptions[2] = "Herbs"                            }
+                        }
+                    
+                    Text("+ 🥛 Milk")
+                        .padding()
+                        .background(selectedOptions.contains("Milk") ? Color(hex: 0xD3D3D3) : Color.white)
+                        .cornerRadius(100)
+                        .onTapGesture {
+                            if selectedOptions.contains("Milk") {
+                                selectedOptions[3] = ""
+                            } else {
+                                self.selectedOptions[3] = "Milk"                            }
+                        }
+                    Spacer()
+                }
+                
+                HStack {
+                    
+                    Text("+ 🐟 Proteins")
+                        .padding()
+                        .background(selectedOptions.contains("Proteins") ? Color(hex: 0xD3D3D3) : Color.white)
+                        .cornerRadius(100)
+                        .onTapGesture {
+                            if selectedOptions.contains("Proteins") {
+                                selectedOptions[4] = ""
+                            } else {
+                                self.selectedOptions[4] = "Proteins"                            }
+                        }
+                    
+                    Spacer()
+                }
+                
+                Text("Selected Options: \(selectedOptions.joined(separator: ", "))")
                     .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.black)
-                    .cornerRadius(10)
+                
+                Button{
+                    
+                } label: {
+                    Text("Done")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.black)
+                        .cornerRadius(10)
+                }
+                .padding()
             }
-            .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .cornerRadius(10)
-        .padding(.vertical)
+            .padding(.horizontal)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .cornerRadius(10)
+            .background(Color(hex: 0xf4f4f4))
 #if os(iOS)
-        .navigationBarTitle("Input Form", displayMode: .inline)
+            .navigationBarTitle("Input Form", displayMode: .inline)
 #endif
+        }
     }
 }
 
 #Preview {
     iOSHomepageView()
+}
+
+extension Color {
+    init(hex: Int, opacity: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: opacity
+        )
+    }
 }
