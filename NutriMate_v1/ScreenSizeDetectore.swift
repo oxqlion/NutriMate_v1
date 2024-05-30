4a27fda7a81d98e650a656cc3839b90cfd55a21a
jhwrwe <126223887+jhwrwe@users.noreply.github.com>
2024-05-30 13:21:11 +0700

    ProfilePage(WIP)
    idk man, send help... please

---
+++ NutriMate_v1/ScreenSizeDetectore.swift
@@ -0,0 +1,14 @@
+//
+//  ScreenSizeDetector.swift
+//  AbsoluteNewFile
+//
+//  Created by MacBook Pro on 29/05/24.
+//
+
+import SwiftUI
+import UIKit
+
+struct ScreenSizeDetector: Codable{
+    var screenWidth = UIScreen.main.bounds.width
+    var screenHeight = UIScreen.main.bounds.height
+}
