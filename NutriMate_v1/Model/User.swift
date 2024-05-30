dfff08d82b34e5d4a0b458ad9614d2cf83d875a4
joren <jalexander05@student.ciputra.ac.id>
2024-05-30 11:48:20 +0700

    joren komputer

---
+++ NutriMate_v1/Model/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  NutriMate_v1
+//
+//  Created by student on 30/05/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class User  {
+var username: String
+    init(username: String) {
+        self.username = username
+      
+    }
+}
+
+
