34a89166e4c61ec95cf00c3139d35f5867b02ff8
jhwrwe <126223887+jhwrwe@users.noreply.github.com>
2024-05-30 11:17:42 +0700

    COmmit FIle Test Push blabla

---
+++ NutriMate_v1/View/ListRecipe.swift
@@ -0,0 +1,18 @@
+//
+//  ListRecipe.swift
+//  NutriMate_v1
+//
+//  Created by MacBook Pro on 30/05/24.
+//
+
+import SwiftUI
+
+struct ListRecipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRecipe()
+}
