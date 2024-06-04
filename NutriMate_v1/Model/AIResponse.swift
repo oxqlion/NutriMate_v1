ee0c63c2ec92092e94c137e7f1acc60926c6dca7
Oxqlion <88870464+oxqlion@users.noreply.github.com>
2024-06-05 03:14:33 +0700

    ai added

---
+++ NutriMate_v1/Model/AIResponse.swift
@@ -0,0 +1,45 @@
+//
+//  AIResponse.swift
+//  NutriMate_v1
+//
+//  Created by zenzen on 04/06/24.
+//
+
+import Foundation
+import OpenAI
+import Combine
+
+class AIResponse: ObservableObject {
+//    private let openAi = OpenAI(apiToken: "sk-proj-ZQUB4mavEpf9YJMsBwdLT3BlbkFJ2Xjzue2caz72r20WRtIP")
+    private let openAi = OpenAI(apiToken: "sk-proj-0CmCQPZJjywktuSapNfsT3BlbkFJC8Y3oOFPOxtdLnNG7t8Z")
+    @Published var responseText: String = ""
+
+    func getReply(prompt: String, completion: @escaping (String) -> Void) {
+        openAi.completions(query: .init(model: .gpt3_5Turbo, prompt: prompt)) { result in
+            switch result {
+            case .success(let response):
+                guard let choice = response.choices.first else {
+                    completion("No response from API")
+                    return
+                }
+                completion(choice.text)
+            case .failure(let error):
+                completion("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+class OpenAIViewModel: ObservableObject {
+    @Published var responseText: String = ""
+    private var aiResponse = AIResponse()
+    private var cancellables = Set<AnyCancellable>()
+
+    func fetchOpenAIResponse(prompt: String) {
+        aiResponse.getReply(prompt: prompt) { [weak self] reply in
+            DispatchQueue.main.async {
+                self?.responseText = reply
+            }
+        }
+    }
+}
